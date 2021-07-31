0d1bc2a34472ea95580fef4c337e5f395d76a379
Swaroop <breadfan18@gmail.com>
2021-07-30 21:11:50 -0400

    test

--- main_app/views.py
+++ main_app/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import ProfileForm
 
-import boto3
+# import boto3
 import uuid
 
 S3_BASE_URL = 'https://s3.us-east-1.amazonaws.com/'
@@ -187,15 +187,16 @@ def dashboard(req):
     })
 
 def add_photo(request, student_id):
-    photo_file = request.FILES.get('photo-file', None)
-    if photo_file:
-        s3 = boto3.client('s3')
-        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
-        try:
-            s3.upload_fileobj(photo_file, BUCKET, key)
-            url = f"{S3_BASE_URL}{BUCKET}/{key}"
-            photo = Photo(url=url, student_id=student_id)
-            photo.save()
-        except:
-            print('An error occurred uploading file to S3')
-    return redirect('students_detail', student_id=student_id)
+    # photo_file = request.FILES.get('photo-file', None)
+    # if photo_file:
+    #     s3 = boto3.client('s3')
+    #     key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+    #     try:
+    #         s3.upload_fileobj(photo_file, BUCKET, key)
+    #         url = f"{S3_BASE_URL}{BUCKET}/{key}"
+    #         photo = Photo(url=url, student_id=student_id)
+    #         photo.save()
+    #     except:
+    #         print('An error occurred uploading file to S3')
+    # return redirect('students_detail', student_id=student_id)
+    pass
